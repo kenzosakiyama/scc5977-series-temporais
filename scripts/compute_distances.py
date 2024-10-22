059e7576f216fc0e15154491027d66c0ce7ec6eb
Kenzo <kenzosakiyama@gmail.com>
2024-10-22 10:34:15 -0300

    Adicionando script que computa distancias separadamente. Adicionando runs iniciais.

--- scripts/compute_distances.py
+++ scripts/compute_distances.py
@@ -2,7 +2,8 @@ from argparse import ArgumentParser
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-from aeon.distances import dtw_pairwise_distance
+from pathlib import Path
+from aeon.distances import dtw_pairwise_distance, ddtw_pairwise_distance, adtw_pairwise_distance
 
 COLUMNS = [
     # 'userAcceleration.x',
@@ -27,20 +28,35 @@ def get_series(df: pd.DataFrame, column: str) -> np.ndarray:
 if __name__ == '__main__':
 
     parser = ArgumentParser()
-    parser.add_argument('--input', type=str, required=True)
+    parser.add_argument('--input', type=str, required=True, help='Input CSV file with the data.')
+    parser.add_argument('--output', type=str, required=True, help='Output folder to store the distances.')
+    parser.add_argument("--distance", type=str, default='dtw', choices=['dtw', 'adtw', 'ddtw'], help='Distance to be used to compute. Must be implemented by aeon.')
     args = parser.parse_args()
 
-    print(f'Computing distances from {args.input}.')
+    output_folder = Path(args.output)
+
+    print(f'- Computing distances from {args.input}.')
+    print(f'- Saving results to {output_folder}.')
+    print(f'- Using {args.distance} distance.')
 
     # Load the data
     data = pd.read_csv(args.input, index_col=0)
 
     for col in tqdm(COLUMNS, desc='Computing distances'):
+        # TODO: da para paralelizar isso
         X = get_series(data, col)
+
+        # Para teste
+        # X = X[:2]
 
         # Compute the distances
         # TODO: no futuro trocar para outra variação do dtw
-        distances = dtw_pairwise_distance(X)
+        if args.distance == 'dtw':
+            distances = dtw_pairwise_distance(X)
+        elif args.distance == 'adtw':
+            distances = adtw_pairwise_distance(X)
+        elif args.distance == 'ddtw':
+            distances = ddtw_pairwise_distance(X)
         
         # Save the distances
-        np.save(f"{col.replace('.', '-')}_distances.npy", distances)
+        np.save(output_folder / f"{col.replace('.', '-')}_distances.npy", distances)
