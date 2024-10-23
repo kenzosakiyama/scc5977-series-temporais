0cfa64ff87a626066837a8099d80d4243dbe5f7d
Kenzo <kenzosakiyama@gmail.com>
2024-10-23 16:12:06 -0300

    Adicionando calculo de distancias multivariadas

--- scripts/compute_distances.py
+++ scripts/compute_distances.py
@@ -12,7 +12,7 @@ COLUMNS = [
     'userAcceleration.z',
 ]
 
-def get_series(df: pd.DataFrame, column: str) -> np.ndarray:
+def get_series(df: pd.DataFrame, column: str, multivar: bool = False) -> np.ndarray:
 
     X = []
 
@@ -23,6 +23,11 @@ def get_series(df: pd.DataFrame, column: str) -> np.ndarray:
             filtered_df = df[subj_mask & act_mask].reset_index()
 
             X.append(filtered_df[column].values)
+
+            if multivar:
+                X.append(
+                    np.stack([filtered_df[col].values for col in COLUMNS])
+                )
     
     return X
 
@@ -32,6 +37,8 @@ if __name__ == '__main__':
     parser.add_argument('--input', type=str, required=True, help='Input CSV file with the data.')
     parser.add_argument('--output', type=str, required=True, help='Output folder to store the distances.')
     parser.add_argument("--distance", type=str, default='dtw', choices=['dtw', 'adtw', 'ddtw'], help='Distance to be used to compute. Must be implemented by aeon.')
+    parser.add_argument("--multivar", action='store_true', help='Use multivariate time series.')
+
     args = parser.parse_args()
 
     output_folder = Path(args.output)
@@ -43,16 +50,25 @@ if __name__ == '__main__':
     # Load the data
     data = pd.read_csv(args.input, index_col=0)
 
-    series = []
 
+    # cada elemento é um conjunto X de séries temporais
+    series = []
     for col in tqdm(COLUMNS, desc='Extracting time series'):
         # TODO: da para paralelizar isso
         X = get_series(data, col)
 
         # Para teste
         # X = X[:2]
-
         series.append(X)
+
+    if args.multivar:
+        # combinando variaveis. a primeira dimensão representa o número de canais
+        print(f'- Using multivariate time series. Columns: {COLUMNS}')
+        multivar_series = []
+        for values in zip(*series):
+            multivar_series.append(np.stack(values))
+        series = multivar_series
+
 
     # TODO: no futuro trocar para outra variação do dtw
     if args.distance == 'dtw':
@@ -62,12 +78,17 @@ if __name__ == '__main__':
     elif args.distance == 'ddtw':
         distance_func = ddtw_pairwise_distance
 
-    # Disparando  e computando as distâncias
-    print(f'- Dispatching {len(COLUMNS)} jobs.')
-    features_distances = Parallel(n_jobs=len(COLUMNS))(
-        delayed(distance_func)(series[i]) for i in range(len(series))
-    )   
+    if not args.multivar:
+        # Disparando e computando as distâncias
+        print(f'- Dispatching {len(COLUMNS)} jobs.')
+        features_distances = Parallel(n_jobs=len(COLUMNS))(
+            delayed(distance_func)(series[i]) for i in range(len(series))
+        )   
 
-    # Salvando distancias
-    for distances, col in zip(features_distances, COLUMNS):
-        np.save(output_folder / f"{col.replace('.', '-')}_distances.npy", distances)
+        # Salvando distancias
+        for distances, col in zip(features_distances, COLUMNS):
+            np.save(output_folder / f"{col.replace('.', '-')}_distances.npy", distances)
+    else:
+        # computa apenas uma vez
+        features_distances = distance_func(series)
+        np.save(output_folder / f"multivar_distances.npy", features_distances)
